fe78b89caa615b6e61249cb6a1a32f9a10150465
sngina <sngina707@gmail.com>
2021-04-21 11:51:44 +0300

    adding Script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 //  the business logic
-function contact (first , last) {
+function contact (first , last ,email ,phone, location) {
 
-this.first = first ;
+this.first = first;
 this.last = last;
+this.email = email;
+this.phone = phone;
+this.location =[];
+
 }
 //the user interface
-$(document)
+$(document).ready(function(){
+ $(".form-group").submit(function(){
+     event.preventDefault();
+     var userFirstName = $("input#firstName").val();
+     var userSecondName = $("input#lastName").val();
+     var userEmail = $("input#email").val();
+     var userNumber = parseInt($("input#phone"));
+     var newContact = new Contact (userFirstName  ,userSecondName);
+    
+ })
+})
